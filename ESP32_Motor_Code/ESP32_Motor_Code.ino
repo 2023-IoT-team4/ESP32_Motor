4fea880f2d4764c57529f24865f6807732e5572a
Serenata2 <astg98@gmail.com>
2023-12-03 13:42:52 +0900

    Refactored code and modified the feedPet function

--- ESP32_Motor_Code/ESP32_Motor_Code.ino
+++ ESP32_Motor_Code/ESP32_Motor_Code.ino
@@ -1,13 +1,19 @@
 /*
-    ESP32_Motor
-
-    반려동물 스마트 홈 프로젝트에서 Motor를 다루는 ESP 코드
+    반려동물 스마트 홈 프로젝트에서 Motor를 다루는 ESP32 보드 코드
 */
 #include <AWS_IOT.h>
 #include <WiFi.h>
 #include <Arduino_JSON.h>
 #include <ESP32_Servo.h>
-#include "config.h"
+
+/***********************************************************************************
+// Wifi
+const char* ssid = "Your WiFi ID";
+const char* password = "Your WiFi Password";
+
+// AWS IoT
+char HOST_ADDRESS[] = "Your AWS IoT End point";
+***********************************************************************************/
 
 // AWS_IOT
 AWS_IOT motor;
@@ -39,14 +45,15 @@ unsigned long lastDebounceTime = 0;      // the last time the output pin was tog
 const unsigned long debounceDelay = 50;  // the debounce time; increase if the output flickers
 int lastButtonState = LOW;               // the previous reading from the input pin
 int buttonState = LOW;                   // the current reading from the input pin
+
 // 유효기간은 5초로 버튼이 클릭된 횟수를 받아온다.
 unsigned long lastButtonClicked = 0;
 const unsigned long clickTimeout = 5000;
 int buttonClicks = 0;
 
 // feeding
-bool isFeedingDone = false;
-int feedCount = 0;
+bool isFeedingDone = false; // 급식 여부를 저장하는 변수
+int feedCount = 0;          // 급식한 횟수를 저장하는 변수
 
 // Ultrasonic Sensor
 const int trigPin = 22;
@@ -60,46 +67,26 @@ void mySubCallBackHandler(char* topicName, int payloadLen, char* payLoad) {
 }
 
 void feedPet(int amount) {
-  Serial.println("함수 호출!!!");
-  isFeedingStart = 1;
+  isFeedingStart = 1;  // Task2에서 led 점멸 시작
+
   if (amount > 0) {
-
     // 180도 모터 회전 시작
-    if (servoPI.read() > 10) {
-      servoPI.write(180);
-      for (int pos = 179; pos >= 0; pos--) {
-        servoPI.write(pos);
-        delay(15);
-      }
-      amount--;
-    }
-    delay(500);
-
-    int i = 0;
-    while (i < amount) {
+    for (int i = 0; i < amount; i++) {
       for (int pos = 0; pos <= 180; pos++) {
         servoPI.write(pos);
         delay(15);
       }
-      i++;
-      delay(500);
-
-      if (i >= amount) {
-        break;
-      }
+      delay(1000);
 
       for (int pos = 180; pos >= 0; pos--) {
         servoPI.write(pos);
-        delay(20);
+        delay(15);
       }
-      i++;
-      delay(500);
+      delay(1000);
     }
-
-    delay(500);
   }
 
-  isFeedingStart = 0;
+  isFeedingStart = 0;    // Task2에서 led 점멸 중지
   isFeedingDone = true;
 }
 
@@ -117,8 +104,8 @@ long measureDistance() {
   //getting duration for echo pulse
   duration = pulseIn(echoPin, HIGH);
 
-  //sound speed = 340 m/s = 34/1000 cm/us
-  //distance = duration * 34/1000 * 1/2
+  // sound speed = 340 m/s = 34/1000 cm/us
+  // distance = duration * 34/1000 * 1/2
   distance = duration * 17 / 1000;
   delay(10);
 
@@ -128,7 +115,7 @@ long measureDistance() {
 void setup() {
   Serial.begin(115200);
 
-  // WiFi
+  // Connecting to WiFi
   Serial.print("WIFI status = ");
   Serial.println(WiFi.getMode());
   WiFi.disconnect(true);
@@ -163,7 +150,7 @@ void setup() {
 
   // attach servo
   servoPI.attach(servoPI_Pin);
-  servoPI.write(0); // 180도 서보모터 초기화
+  servoPI.write(0);  // 180도 서보모터 초기화
 
   // 초음파 센서
   pinMode(trigPin, OUTPUT);
@@ -222,19 +209,22 @@ void Task1code(void* pvParameters) {
       if (amount > 0) {
         feedCount = amount;
         feedPet(amount);
-        buttonClicks = 0; // 버튼을 클릭하는 도중에 명령이 왔다면 초기화해주기
+        buttonClicks = 0;  // 버튼을 클릭하는 도중에 명령이 왔다면 초기화해주기
       }
-    } 
-    else {
+    } else {
       unsigned long currentTime = millis();
+
+      // 버튼이 마지막으로 클릭된 후 5초가 지나면 급식 시작
       if (buttonClicks > 0 && (currentTime - lastButtonClicked) > clickTimeout) {
         buttonClicks = min(buttonClicks, 10);  // 최대 10번 feeding 진행 가능
         feedCount = buttonClicks;
         buttonClicks = 0;
         feedPet(feedCount);
       } else {
+        // 스위치가 클릭될 때 발생하는 Debounce 문제 해결
+        // 버튼의 상태가 변화되고, 변화 후 50ms 이상 지속될 시 클릭되었다고 판단
         int reading = digitalRead(buttonPin);
-        //Serial.println(reading);
+        
         if (reading != lastButtonState) {
           lastDebounceTime = currentTime;
         }
@@ -242,10 +232,9 @@ void Task1code(void* pvParameters) {
           if (reading != buttonState) {
             buttonState = reading;
 
-            // button이 클릭될 시
             if (buttonState == HIGH) {
               Serial.println("버튼이 클릭됨!!!");
-              buttonClicks++;  // feed_amount 증가
+              buttonClicks++;    
               lastButtonClicked = currentTime;
             }
           }
@@ -255,21 +244,20 @@ void Task1code(void* pvParameters) {
     }
 
     if (isFeedingDone) {
-      Serial.print("clicked amount : ");
+      Serial.print("feed amount : ");
       Serial.println(feedCount);
 
       long dis = measureDistance();
       Serial.print("distance : ");
       Serial.println(dis);
 
-      if(dis > 10) {
-        sprintf(payload, "{\"state\": { \"reported\": {\"feed_given\": true, \"feed_enough\": false}}}", feedCount);
-      }
-      else{
-        sprintf(payload, "{\"state\": { \"reported\": {\"feed_given\": true, \"feed_enough\": true}}}", feedCount);
-      }
+      // 높이의 비율을 이용해서 남은 사료량의 %를 구했다. 하지만 실제 사료통은 원뿔 형태로
+      // 높이와 반지름간에 비율과 원뿔 부피 공식을 이용해서 구해야지 정확하겠다.
+      float remaining_percentage = min(100.0f, ((17.0f - (float)dis) / 17.0f) * 100.0f);
+      sprintf(payload, "{\"state\": { \"reported\": {\"feed_given\": true, \"feed_enough\": %.2f}}}", remaining_percentage);
 
-      while(motor.publish(pTOPIC_NAME, payload) != 0){
+      // topic에 publish 될 때까지 진행하기
+      while (motor.publish(pTOPIC_NAME, payload) != 0) {
         Serial.println("Feeding Done Publish failed");
         delay(10);
       }
@@ -288,6 +276,7 @@ void Task2code(void* pvParameters) {
   Serial.println(xPortGetCoreID());
 
   for (;;) {
+    // 모터가 돌면서 급식이 시작되면 동시에 led 점멸 시작하기
     if (isFeedingStart) {
       digitalWrite(led, LOW);
       delay(1000);
